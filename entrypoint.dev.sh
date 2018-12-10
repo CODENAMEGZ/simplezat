dc424b84b13b8eb683ca14a33c310a4129be40da
Jessadakorn Akarapaiboonroj <jessadakorn@prontomarketing.com>
2018-12-10 20:34:05 +0700

    test7

--- entrypoint.dev.sh
+++ entrypoint.dev.sh
@@ -2,5 +2,5 @@
 
 cd simplezat
 pipenv run python manage.py migrate --setting=simplezat.settings.dev
-pipenv run  python manage.py collectstatic --noinput --setting=simplezat.settings.dev
+pipenv run python manage.py collectstatic --noinput --setting=simplezat.settings.dev
 pipenv run uwsgi --ini uwsgi.dev.ini
